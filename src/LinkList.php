27778daeedb09dcf1d17e773e78cbe4e7c8b5345
cantu <tusion@163.com>
2015-04-01 11:00:06 +0800

    增加了链表查找和反转的方法

--- src/LinkList.php
+++ src/LinkList.php
@@ -10,6 +10,11 @@ namespace tusion\algorithms;
 
 require_once './LinkNode.php';
 require_once './Utils.php';
+
+/**
+ * Class LinkList
+ * @package tusion\algorithms
+ */
 class LinkList {
 
     protected $header ;
@@ -76,6 +81,7 @@ class LinkList {
 
     /**
      * 删除链表中的某一个元素
+     * 这种遍历算法的时间复杂度 O(n/2)
      * @param $data
      * @return bool
      */
@@ -107,6 +113,17 @@ class LinkList {
         }
         return false;
 
+    }
+
+    /**
+     * TODO:O(1)的时间内删除链表中的一个元素;
+     * 当需要删除节点的指针时,我们可以把下溢节点的内容复制到这个节点,然后释放下一节点即可.
+     * <<剑指offer>>13题
+     * @param $data
+     */
+    function del_node_fast( $data )
+    {
+        ;
     }
 
     /**
@@ -207,6 +224,26 @@ class LinkList {
     }
 
     /**
+     *反转打印链表元素,链表保持原样.
+     *TODO:用栈实现反转打印
+     * <<剑指offer>>第5题
+     */
+    function to_reversal_string_use_stack()
+    {
+        ;
+    }
+
+    /**
+     *反转打印链表元素,链表保持原样.
+     *TODO:用递归实现反转打印
+     * <<剑指offer>>第5题
+     */
+    function to_reversal_string_use_recursion()
+    {
+        ;
+    }
+
+    /**
      * 获取当前链表长度
      * @return int
      */
@@ -266,12 +303,7 @@ class LinkList {
     }
 
 
-    //TODO 测试一个链表是否有环
-    function cycle_detect( $list )
-    {
-        ;
 
-    }
 
     function set_debug( $is_debug)
     {
@@ -284,6 +316,120 @@ class LinkList {
             self::$is_debug = false;
         }
     }
+
+    /**
+     * 输入一个链表,返回链表中倒数第K个节点,不用两遍遍历链表.
+     * 定义两个相距K-1的指针,移动步长都是1,头指针到K-1个节点时,两个指针一起向前走.
+     * 当头指针到队尾时,后面的指针在倒数第K个元素
+     * 比如链表为:1,2,3,4,5,6, 输出倒数第3个节点,就输出4
+     * <<剑指offer>>15题
+     * @param $last_k: 倒数第K个元素.
+     * @return null
+     */
+    function get_last_k_node(  $last_k )
+    {
+        if($this->header=== null )
+        {
+            echo"Error: this link list is null.\n";
+            return null;
+        }
+        if( $last_k <= 0)
+        {
+            echo"Error: input last k should be bigger than 0 \n";
+            return null;
+        }
+
+        $ahead_pointer = $this->header;
+        $behind_pointer = $this->header;
+        for($i=0; $i<$last_k; $i++)
+        {
+            //链表长度没有K值那么长
+            if( $ahead_pointer === null )
+            {
+                echo"Error: this link list length just ".($i+1).", input K is too large.\n";
+                return null;
+            }
+            $ahead_pointer = $ahead_pointer->next;
+        }
+        //两个指针相距(K-1)时,一起向前移动,直到头指针到达队尾.
+        while( $ahead_pointer!== null )
+        {
+            $ahead_pointer = $ahead_pointer->next;
+            $behind_pointer = $behind_pointer->next;
+        }
+        return $behind_pointer->data;
+    }
+
+    /**
+     * TODO:查找链表的中间节点
+     * 定义两个指针,从头节点开始,一个步长为1,一个步长为2
+     * 则当走的快的指针到链表尾端是,慢的指针则是中间节点
+     */
+    function get_mid_node()
+    {
+        ;
+    }
+
+
+    /**
+     * TODO::合并两个排列有序的链表
+     * <<剑指offer>>17题
+     * @param $list:需要合并的另外一个链表
+     */
+    function merge_list( $list )
+    {
+        ;
+    }
+    /**
+     * TODO:测试一个链表是否有环
+     * 定义两个指针,从头节点开始,一个步长为1,一个步长为2
+     * 如果走的快的指针和走的慢的指针相遇,则有环行链表.
+     * 如果走的快的指针走到队尾都还没有和慢指针相遇,则没有环.
+     */
+    function is_cycle( )
+    {
+        ;
+
+    }
+
+
+
+    /**
+     * 反转链表
+     * 只要一个一个O(2)的辅助变量存住上一个和下一个节点,
+     * <<剑指offer>>16题
+     * @return null
+     */
+    function reversal()
+    {
+        if( $this->header === null )
+        {
+            echo"Error: this link list is null\n";
+            return null;
+        }
+        if( $this->get_length() === 1)
+        {
+            return $this->header;
+        }
+
+        $pre_node = $this->header;
+        $node = $pre_node->next;
+        while( $node !== null )
+        {
+            $next_node = $node->next;
+            $node->next = $pre_node;
+            $pre_node = $node;
+            $node = $next_node;
+        }
+
+        $this->header->next = null;
+        $this->header = $pre_node;
+        return $this->header;
+    }
+
+
+
+
     /**
      * 单元测试
      */
@@ -348,6 +494,39 @@ class LinkList {
        // $list->to_string();
         $util->probe_end();
         $util->probe_result();
+
+        //查找倒数第K个数
+        echo "\nget last k data:\n";
+        $list->clear_list();
+        for( $i=0; $i<7; $i++)
+        {
+            $list->add_node( $i );
+        }
+        $list->to_string();
+        $last_k_data = $list->get_last_k_node(3 );
+        echo "k=3, data = ".$last_k_data."\n";
+        $last_k_data = $list->get_last_k_node(1);
+        echo "k=1, data = ".$last_k_data."\n";
+        $last_k_data = $list->get_last_k_node(7);
+        echo "k=7, data = ".$last_k_data."\n";
+        $last_k_data = $list->get_last_k_node(8);
+        echo "k=8, data = ".$last_k_data."\n";
+
+        //反转链表
+        echo"\nreversal link list \n";
+        $list->to_string();
+        $list->reversal();
+        echo"reversal: ";
+        $list->to_string();
+        $list->clear_list();
+        //$list->add_node(1);
+        //$list->add_node(2);
+        $list->to_string();
+        $list->reversal();
+        echo"reversal: ";
+        $list->to_string();
+
+
 
     }
 
